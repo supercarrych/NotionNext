6d3626344b3fa13bb1a56623a9ecfe82ff0914b1
supercarrych <71623161+supercarrych@users.noreply.github.com>
2023-06-19 21:01:54 +0100

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,2 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6936277554834045"
-     crossorigin="anonymous"></script>
+
